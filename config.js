41af3053c9e16a9543318ba0f1cc2ed4eb6d6c7d
Muhammad Aqil Ismail <97868843+BlankedWave@users.noreply.github.com>
2025-02-13 21:52:17 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
+  activityName: "Hitz Yang Terkini!", 
   activityType: "STREAMING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/X6RT5VdJPQ",
   embedTimeout: 5, 
