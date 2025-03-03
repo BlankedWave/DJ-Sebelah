330722930a166da756f7993b5cd0a7d00229bf74
Muhammad Aqil Ismail <97868843+BlankedWave@users.noreply.github.com>
2025-03-03 22:40:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ module.exports = {
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityName: "Lagu Hitz Yang Terkini!", 
+  activityType: "STREAMING",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/X6RT5VdJPQ",
   embedTimeout: 5, 
   errorLog: "", 
