77e99270367ad1a6f28c9cc53525f20bd178890c
Muhammad Aqil Ismail <97868843+BlankedWave@users.noreply.github.com>
2025-03-22 22:53:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -17,11 +17,11 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "Disutils Team Lavalink 1",
-      password: "https://disutils.com",
-      host: "lavalink-1.is-it.pink",
-      port:  443,
-      secure: true
+      name: "Lavalink",
+      password: "jmlitelavalink",
+      host: "46.202.82.164",
+      port:  1027,
+      secure: false
     }
   ]
 }
