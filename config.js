626ff17aad247f41725986feb402ae4a1b69ea48
Muhammad Aqil Ismail <97868843+BlankedWave@users.noreply.github.com>
2025-05-19 17:44:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "Jirayu",
-      password: "youshallnotpass",
-      host: "lavalink.jirayu.net",
-      port:  13592,
+      name: "GlaceYT",
+      password: "glaceyt",
+      host: "193.226.78.187",
+      port:  3543,
       secure: false
     }
   ]
